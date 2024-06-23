0bfc9e5a588214a7ba605f53322b1de18bbc1faf
Barış SEMERCİ <barissemerci3@gmail.com>
2024-06-23 20:16:55 +0300

    add AndroidEntryPoint annotation and add GenderScreen

--- app/src/main/java/com/partnercodes/calorytracker/MainActivity.kt
+++ app/src/main/java/com/partnercodes/calorytracker/MainActivity.kt
@@ -9,8 +9,11 @@ import androidx.navigation.compose.rememberNavController
 import com.partnercodes.calorytracker.navigation.navigate
 import com.partnercodes.calorytracker.ui.theme.CaloryTrackerTheme
 import com.partnercodes.core.navigation.Route
+import com.partnercodes.onboarding_presentation.gender.GenderScreen
 import com.partnercodes.onboarding_presentation.welcome.WelcomeScreen
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +31,7 @@ class MainActivity : ComponentActivity() {
 
                     }
                     composable(Route.GENDER) {
+                        GenderScreen(onNavigate = navController::navigate)
 
                     }
                     composable(Route.HEIGHT) {
